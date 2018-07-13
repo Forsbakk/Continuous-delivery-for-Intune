af4f4492268144cba7f30766f60460587bbd4c03
Forsbakk <jonas.forsbakk@horten.kommune.no>
2018-07-13 09:23:18 +0200

    test

--- versioncontrol/Create-Config.ps1
+++ versioncontrol/Create-Config.ps1
@@ -1,18 +1,20 @@
-﻿$Versions = @(
-    @{
-        Name = "prod.bs"
-        Version = "0.0.1"
-        File = "Script.prod.us.ps1"
-    },
-    @{
-        Name = "prod.us"
-        Version = "0.0.1"
-        File = "Script.prod.us.ps1"
-    },
-    @{
-        Name = "beta"
-        Version = "0.0.1"
-        File = "Script.beta.ps1"
-    }
-)
+﻿$Versions = @{
+    Products = @(
+        @{
+            Name    = "prod.bs"
+            Version = "0.0.1"
+            File    = "Script.prod.us.ps1"
+        },
+        @{
+            Name    = "prod.us"
+            Version = "0.0.1"
+            File    = "Script.prod.us.ps1"
+        },
+        @{
+            Name    = "beta"
+            Version = "0.0.1"
+            File    = "Script.beta.ps1"
+        }
+    )
+}
 $Versions | ConvertTo-Json -Compress | Out-File config.json
